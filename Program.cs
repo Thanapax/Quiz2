5b8b22c07b3ec7c13967a72adf4445b268339639
Thanapa <eve.jitthana@gmail.com>
2023-04-03 10:33:38 +0700

    -

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿class program {
+    static void Main(string[] args){
+        StatisticNum();
+    }
+    static double StatisticNum(double num){
+        Console.Write("Pls input number : ");
+        int i = 0;
+        for(int j = 0; j == i ; j++){
+            double number = double.Parse(Console.ReadLine());
+        } return Messege();
+    }
+    static double MaxMinMean(double number){
+        return (StatisticNum(number) + number) / number ;
+    }
+    static void Messege(){
+        string messege = Console.ReadLine();
+        if (messege == "End"){
+            if (messege == "Findmax"){
+                return  ();
+            } else if (messege == "Findmin"){
+                return ();
+            } else if (messege == "Findmean"){
+                return MaxMinMean ;
+            } else {
+                Console.WriteLine("Invalid mode");
+            }
+        }
+        
+    }
+}
